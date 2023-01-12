3cfcdb60422899f51fffe80b7f1243af3076e610
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-01-12 20:11:18 +0600

    upload

---
+++ README.md
@@ -0,0 +1,16 @@
+## Change Background Color By Clicking Or Giving Input Hex Color code
+****
+
+### Using Tools
+* HTML:5
+* Tailwind CSS
+* JavaScript
+
+### Using JavaScript
+* setInterval
+* Math.floor
+* Math.random
+* .toString(16) [for decimal to hexadecimal code]
+
+### Live Website Link
+<a href="https://rejoyanislam.github.io/change-background-color-by-giving-input-hex-code-or-click-change-color-button/">Click</a>
---
+++ app.js
@@ -0,0 +1,56 @@
+const copy = document.getElementById("copy");
+const input = document.getElementById("input");
+const showResult = document.getElementById("showResult");
+const showError = document.getElementById("showError");
+
+// hex color generate function
+const hexColor = () => {
+  const red = Math.floor(Math.random() * 255).toString(16);
+  const green = Math.floor(Math.random() * 255).toString(16);
+  const blue = Math.floor(Math.random() * 255).toString(16);
+  return `#${red}${green}${blue}`;
+};
+
+input.oninput=(e)=>{
+e.target.value= e.target.value.toUpperCase()
+}
+
+ input.onkeyup=(e)=>{
+const value= e.target.value;
+if (patternCheck(value)) {
+  document.querySelector("body").style.backgroundColor = `#${value}`;
+} else {
+  showError.classList.remove("hidden");
+  showError.innerHTML = `Wrong Hex code`;
+}
+}
+
+
+const change = () => {
+  const hexColorCode=hexColor().toUpperCase()
+document.querySelector("body").style.backgroundColor = hexColorCode;
+input.value=`${hexColorCode.split('#')[1]}`
+
+};
+
+// pattern check function
+function patternCheck(data) {
+  const pattern = /^[0-9abcdef]{3,6}$/i;
+  const result = pattern.test(data);
+  return result;
+}
+
+// set setInterval for show limited time output
+setInterval(() => {
+  showError.classList.add("hidden");
+  showResult.classList.add("hidden");
+}, 2000);
+
+// code copy
+copy.onclick = () => {
+  const value = input.value;
+  if (patternCheck(value)) {
+    showResult.classList.remove("hidden");
+    showResult.innerHTML =     value ? `#${value} copied` : "#bae6fd copied" ;
+  }
+};
